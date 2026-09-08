48d4b93d689f6acd7590d7e36850960ace874f82
noah <noahdmiedema@icloud.com>
2026-09-08 19:21:42 -0400

    updated tutorial

--- Tutorial.py
+++ Tutorial.py
@@ -2,11 +2,35 @@
 from codrone_edu.drone import *
 import time
 
-#connect the drone and declare what the drone is for the library
+# connect the drone and declare what the drone is for the library
 drone = Drone()
 drone.connect()
 
-drone.takeoff()
+# python automatically defines variable type like int, string, or double
+delay = 0.2
 
-while True:
-    pass
+# First go through the try block
+try:
+    drone.takeoff()
+    while True:
+        # square
+        # distance, unit ("cm" or 'in'), speed (0 to 1)
+        drone.move_forward(10, 'in', 0.5)
+        # delay in seconds
+        time.sleep(delay)
+        drone.move_left(10, 'in', 0.5)
+        time.sleep(delay)
+        drone.move_backward(10, 'in', 0.5)
+        time.sleep(delay)
+        drone.move_right(10, 'in', 0.5)
+        # break the loop
+        break
+
+# if an error is caught always run this block
+except Exception as e:
+    print(e)
+
+#then no matter the outcome of the try block, run this:
+finally:
+    drone.land()
+    drone.close()
